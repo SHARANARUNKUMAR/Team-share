25709b037604be40b94229f51a6e447afb7705e8
Rupika <Rupika@Dell>
2025-07-25 13:54:27 +0530

    updated

--- C2TC_Java/src/com/mvit/github/Main.java
+++ C2TC_Java/src/com/mvit/github/Main.java
@@ -2,10 +2,11 @@ package com.mvit.github;
 
 public class Main {
 
-	public static void main(String[] args) {
+	static public void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MVIT - C66");
 		System.out.println("Hello From Sharan");
+		System.out.println("HII");
 
 	}
 
