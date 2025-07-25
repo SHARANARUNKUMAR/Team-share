88928bdeda18d1e887a0cc1bd7c4d1b738e546dd
praveenkumardev-tech <tamizhprem70@gmail.com>
2025-07-25 14:34:35 +0530

    MVIT is changed into TNSIF

--- C2TC_Java/src/com/mvit/github/Main.java
+++ C2TC_Java/src/com/mvit/github/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	static public void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("MVIT - C66");
+		System.out.println("TNSIF - C66");
 		System.out.println("Hello From Sharan");
 		System.out.println("HII");
 		System.out.println("SHARAN");
